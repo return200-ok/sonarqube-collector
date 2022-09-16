8c66a5d8449fc5cf3ce8ed031c9fe4b83ed52178
caolv <caolv@biplus.com.vn>
2022-09-16 13:49:28 +0700

    m

--- sonarqube-crawler.py
+++ sonarqube-crawler.py
@@ -1,8 +1,5 @@
-import scrapy
 from influxdb import InfluxDBClient
 from time import time
-import pdb
-import random
 import json
 import requests
 
@@ -16,40 +13,43 @@ def getData(url, token):
   return data
 
 def parseData(component, metricKeys, token):
-  url = "https://sonarqube.biplus.com.vn/api/measures/component?component="+component+"&metricKeys="+metricKeys
+  url = "https://sonarqube.x.com.vn/api/measures/component?component="+component+"&metricKeys="+metricKeys
   return getData(url, token)
 
 
-def insertData(metric, project_key, name, value):
-  db = InfluxDBClient("localhost", 8086)
+def insertData(metricKey, project_key, name, value):
+  db = InfluxDBClient(host="localhost", port=8086, username="caolv", password="admin1998")
   db.switch_database("sonarqube_kpi")
   kpi = [{
-    "measurement": metric,
+    "measurement": metricKey,
     "tags": {
       "project_key": project_key,
       "name": name
     },
     "time": int(time()) * 1000000000,
     "fields": {
-      "loc": str(float(value))
+      "value": str(value)
     }
   }]
   db.write_points(kpi)
 
 if __name__ == '__main__':
   token = "squ_a4c1bd1d9be8613d1179234e634d7541af960f2b"
-  output = getData("https://sonarqube.biplus.com.vn/api/components/search?qualifiers=TRK", token)
-  metricKeys = ["ncloc", "bugs", "code_smells", "sqale_index"]
-  for components in output['components']:
-    for metricKey in metricKeys:
-      data = parseData((components['key']), metricKey, token)
+  list_component = getData("https://sonarqube.x.com.vn/api/components/search?qualifiers=TRK", token)
+  list_metricKeys = getData("https://sonarqube.x.com.vn/api/metrics/search", token)
+  for components in list_component['components']:
+    for metricKey in list_metricKeys['metrics']:
+      data = parseData((components['key']), (metricKey['key']), token)
       project_key = data["component"]["key"]
       name = data["component"]["name"]
+      value = ""
       if len(data["component"]["measures"]) == 0:
-        value = 0
-      else: 
+        value = "null"
+      elif "value" in data["component"]["measures"][0]: 
         value = data["component"]["measures"][0]["value"]
-      insertData(metricKey, project_key, name, value)
+      else:
+        print("Key doesn't exist in JSON data")
+      insertData((metricKey['key']), project_key, name, value)
 
 
 
--- test.py
+++ test.py
@@ -1,48 +1,60 @@
-import scrapy
-from influxdb import InfluxDBClient
 from time import time
-import pdb
-import random
 import json
 import requests
+from datetime import datetime
+
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 
-url = 'https://sonarqube.biplus.com.vn/api/measures/component?component=Test&metricKeys=ncloc'
-myToken = 'squ_a4c1bd1d9be8613d1179234e634d7541af960f2b'
-session = requests.Session()
-session.auth = myToken, ''
-call = getattr(session, 'get')
-res = call(url)
-output = json.loads(res.content)
-project_key=output["component"]["key"]
-loc=output["component"]["measures"][0]["value"]
-db = InfluxDBClient("localhost", 8086)
-db.switch_database("sonarqube_kpi")
-kpi = [{
-  "measurement": "loc",
-  "tags": {
-    "project_key": "Test",
-    "name": "authorization-service"
-  },
-  "time": int(time()) * 1000000000,
-  "fields": {
-    "loc": str(float(loc))
-  }
-}]
-db.write_points(kpi)
+def getData(url, token):
+  session = requests.Session()
+  session.auth = token, ''
+  call = getattr(session, 'get')
+  res = call(url)
+  data = json.loads(res.content)
+  return data
 
-def parse_args(project_key, name, value):
+def parseData(component, metricKeys, token):
+  url = "https://sonarqube.x.com.vn/api/measures/component?component="+component+"&metricKeys="+metricKeys
+  return getData(url, token)
+
+
+def insertData(metric, project_key, name, value):
+  token = "owNcFjQ_CYRqF17x8Nlt0ByUdGg4xMar51ziPJ7murTdoptCvy6BBwUYwHWL7JdPmWD7qsc4y1FgKRqGrCrPVw=="
+  org = "mainorg"
+  url = "http://localhost:8086"
+  bucket = "sonarqube_kpi"
+  client = InfluxDBClient(url=url, token=token, org=org)
+
   kpi = [{
-    "measurement": "loc",
+    "measurement": metric,
     "tags": {
-      "project_key": "Test",
-      "name": "authorization-service"
+      "project_key": project_key,
+      "project_name": name
     },
     "time": int(time()) * 1000000000,
     "fields": {
-      "loc": str(float(loc))
+      metric: str(value)
     }
   }]
+  write_api = client.write_api(write_options=SYNCHRONOUS)
+  write_api.write(bucket, org, kpi)
+  print ("write ", kpi," to bucket "+bucket)
 
 
 
+if __name__ == '__main__':
+  token = "squ_a4c1bd1d9be8613d1179234e634d7541af960f2b"
+  output = getData("https://sonarqube.x.com.vn/api/components/search?qualifiers=TRK", token)
+  metricKeys = ["ncloc", "bugs", "code_smells", "sqale_index"]
+  for components in output['components']:
+    for metricKey in metricKeys:
+      data = parseData((components['key']), metricKey, token)
+      project_key = data["component"]["key"]
+      name = data["component"]["name"]
+      if len(data["component"]["measures"]) == 0:
+        value = 0
+      else: 
+        value = data["component"]["measures"][0]["value"]
+      insertData(metricKey, project_key, name, value)
